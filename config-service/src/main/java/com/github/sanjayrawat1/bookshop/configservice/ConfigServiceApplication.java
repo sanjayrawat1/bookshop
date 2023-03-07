1cfe1f8402774328062bffdb2d5c46690f51979c
Sanjay Singh Rawat <s.rawat1339@gmail.com>
2023-03-07 18:08:39 +0530

    config-service: configure interaction between config server and config repo

--- config-service/src/main/java/com/github/sanjayrawat1/bookshop/configservice/ConfigServiceApplication.java
+++ config-service/src/main/java/com/github/sanjayrawat1/bookshop/configservice/ConfigServiceApplication.java
@@ -2,8 +2,11 @@ package com.github.sanjayrawat1.bookshop.configservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+// Activates the configuration server implementation in the Spring Boot application
+@EnableConfigServer
 public class ConfigServiceApplication {
 
     public static void main(String[] args) {
