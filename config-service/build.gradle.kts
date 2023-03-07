034859c9d27476deefa6d59a846bbb0b14ce0de4
Sanjay Singh Rawat <s.rawat1339@gmail.com>
2023-03-07 17:53:00 +0530

    config-service: initial commit

---
+++ config-service/build.gradle.kts
@@ -0,0 +1,57 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.0.4"
+    id("io.spring.dependency-management") version "1.1.0"
+    id("com.diffplug.spotless") version "6.16.0"
+}
+
+group = "com.github.sanjayrawat1.bookshop"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_19
+
+repositories {
+    mavenCentral()
+}
+
+extra["springCloudVersion"] = "2022.0.1"
+
+dependencies {
+    implementation("org.springframework.cloud:spring-cloud-config-server")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+tasks.build {
+    dependsOn("spotlessApply")
+}
+
+spotless {
+    java {
+        toggleOffOn()
+        importOrder()
+        removeUnusedImports()
+        cleanthat()
+        prettier(mapOf("prettier" to "2.8.4", "prettier-plugin-java" to "2.0.0"))
+            .config(mapOf("parser" to "java", "printWidth" to 160, "tabWidth" to 4, "useTabs" to false))
+        formatAnnotations()
+    }
+    kotlinGradle {
+        target("*.gradle.kts")
+        ktlint()
+    }
+    format("misc") {
+        target("*.gradle", "*.md", ".gitignore")
+        trimTrailingWhitespace()
+        indentWithSpaces()
+        endWithNewline()
+    }
+}
---
+++ config-service/src/main/java/com/github/sanjayrawat1/bookshop/configservice/ConfigServiceApplication.java
@@ -0,0 +1,12 @@
+package com.github.sanjayrawat1.bookshop.configservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServiceApplication.class, args);
+    }
+}
