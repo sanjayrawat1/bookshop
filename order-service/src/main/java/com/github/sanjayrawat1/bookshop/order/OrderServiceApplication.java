21ad0129201ba7f11e171f17dd48076b27ab0770
Sanjay Singh Rawat <s.rawat1339@gmail.com>
2023-03-15 14:28:22 +0530

    order-service: initial commit

---
+++ order-service/src/main/java/com/github/sanjayrawat1/bookshop/order/OrderServiceApplication.java
@@ -0,0 +1,12 @@
+package com.github.sanjayrawat1.bookshop.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderServiceApplication.class, args);
+    }
+}
---
+++ order-service/src/test/java/com/github/sanjayrawat1/bookshop/order/OrderServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.github.sanjayrawat1.bookshop.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
