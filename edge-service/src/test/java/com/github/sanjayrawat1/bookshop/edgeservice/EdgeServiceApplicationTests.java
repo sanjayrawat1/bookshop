95f4196ccbd9ed87e3083b5c696c363253a96352
Sanjay Singh Rawat <s.rawat1339@gmail.com>
2023-03-18 20:44:25 +0530

    edge-service: initial commit

---
+++ edge-service/src/test/java/com/github/sanjayrawat1/bookshop/edgeservice/EdgeServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.github.sanjayrawat1.bookshop.edgeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdgeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
