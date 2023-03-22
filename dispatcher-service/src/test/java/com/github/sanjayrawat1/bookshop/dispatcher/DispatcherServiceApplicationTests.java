2c3b6889a4cc11409e480e37a0fbffddbb1b8df8
Sanjay Singh Rawat <s.rawat1339@gmail.com>
2023-03-22 12:03:42 +0530

    dispatcher-service: initial commit

---
+++ dispatcher-service/src/test/java/com/github/sanjayrawat1/bookshop/dispatcher/DispatcherServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.github.sanjayrawat1.bookshop.dispatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DispatcherServiceApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
