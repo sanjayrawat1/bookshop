664fe6870c720c4883eec91f5919e0bf81914a8b
Sanjay Singh Rawat <s.rawat1339@gmail.com>
2023-03-28 11:01:59 +0530

    bookshop-deployment: update cluster creation script

--- bookshop-deployment/kubernetes/platform/development/create-cluster.sh
+++ bookshop-deployment/kubernetes/platform/development/create-cluster.sh
@@ -10,9 +10,29 @@ minikube addons enable ingress --profile bookshop
 
 sleep 15
 
-echo "\n📦 Deploying platform services..."
+echo "\n📦 Deploying Keycloak..."
 
-kubectl apply -f services
+kubectl apply -f services/keycloak-config.yml
+kubectl apply -f services/keycloak.yml
+
+sleep 5
+
+echo "\n⌛ Waiting for Keycloak to be deployed..."
+
+while [ $(kubectl get pod -l app=bookshop-keycloak | wc -l) -eq 0 ] ; do
+  sleep 5
+done
+
+echo "\n⌛ Waiting for Keycloak to be ready..."
+
+kubectl wait \
+  --for=condition=ready pod \
+  --selector=app=bookshop-keycloak \
+  --timeout=300s
+
+echo "\n📦 Deploying PostgreSQL..."
+
+kubectl apply -f services/postgresql.yml
 
 sleep 5
 
@@ -29,6 +49,12 @@ kubectl wait \
   --selector=db=bookshop-postgres \
   --timeout=180s
 
+echo "\n📦 Deploying Redis..."
+
+kubectl apply -f services/redis.yml
+
+sleep 5
+
 echo "\n⌛ Waiting for Redis to be deployed..."
 
 while [ $(kubectl get pod -l db=bookshop-redis | wc -l) -eq 0 ] ; do
@@ -42,6 +68,12 @@ kubectl wait \
   --selector=db=bookshop-redis \
   --timeout=180s
 
+echo "\n📦 Deploying RabbitMQ..."
+
+kubectl apply -f services/rabbitmq.yml
+
+sleep 5
+
 echo "\n⌛ Waiting for RabbitMQ to be deployed..."
 
 while [ $(kubectl get pod -l db=bookshop-rabbitmq | wc -l) -eq 0 ] ; do
@@ -53,6 +85,25 @@ echo "\n⌛ Waiting for RabbitMQ to be ready..."
 kubectl wait \
   --for=condition=ready pod \
   --selector=db=bookshop-rabbitmq \
+  --timeout=180s
+
+echo "\n📦 Deploying bookshop UI..."
+
+kubectl apply -f services/bookshop-ui.yml
+
+sleep 5
+
+echo "\n⌛ Waiting for Bookshop UI to be deployed..."
+
+while [ $(kubectl get pod -l app=bookshop-ui | wc -l) -eq 0 ] ; do
+  sleep 5
+done
+
+echo "\n⌛ Waiting for Bookshop UI to be ready..."
+
+kubectl wait \
+  --for=condition=ready pod \
+  --selector=app=bookshop-ui \
   --timeout=180s
 
 echo "\n⛵ Happy Sailing!\n"
