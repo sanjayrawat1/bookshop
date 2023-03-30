413d48346755104199a7ece7ec0b3921edffb43e
Sanjay Singh Rawat <s.rawat1339@gmail.com>
2023-03-30 13:43:24 +0530

    catalog-service: configure log pattern and add logs

--- catalog-service/src/main/java/com/github/sanjayrawat1/bookshop/catalog/web/rest/BookController.java
+++ catalog-service/src/main/java/com/github/sanjayrawat1/bookshop/catalog/web/rest/BookController.java
@@ -4,6 +4,7 @@ import com.github.sanjayrawat1.bookshop.catalog.domain.Book;
 import com.github.sanjayrawat1.bookshop.catalog.domain.BookService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author Sanjay Singh Rawat
  */
+@Slf4j
 @RestController
 @RequestMapping("/books")
 @RequiredArgsConstructor
@@ -29,28 +31,33 @@ public class BookController {
 
     @GetMapping
     public Iterable<Book> get() {
+        log.info("Fetching the list of books in the catalog");
         return bookService.viewBookList();
     }
 
     @GetMapping("/{isbn}")
     public Book getByIsbn(@PathVariable String isbn) {
+        log.info("Fetching the book with ISBN {} from the catalog", isbn);
         return bookService.viewBookDetails(isbn);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Book post(@Valid @RequestBody Book book) {
+        log.info("Adding a new book to the catalog with ISBN {}", book.isbn());
         return bookService.addBookToCatalog(book);
     }
 
     @DeleteMapping("{isbn}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable String isbn) {
+        log.info("Deleting book with ISBN {}", isbn);
         bookService.removeBookFromCatalog(isbn);
     }
 
     @PutMapping("{isbn}")
     public Book put(@PathVariable String isbn, @Valid @RequestBody Book book) {
+        log.info("Updating book with ISBN {}", isbn);
         return bookService.editBookDetails(isbn, book);
     }
 }
--- catalog-service/src/main/java/com/github/sanjayrawat1/bookshop/catalog/web/rest/HomeController.java
+++ catalog-service/src/main/java/com/github/sanjayrawat1/bookshop/catalog/web/rest/HomeController.java
@@ -2,12 +2,16 @@ package com.github.sanjayrawat1.bookshop.catalog.web.rest;
 
 import com.github.sanjayrawat1.bookshop.catalog.config.BookshopProperties;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
+ * Home Controller.
+ *
  * @author Sanjay Singh Rawat
  */
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class HomeController {
@@ -16,6 +20,7 @@ public class HomeController {
 
     @GetMapping("/")
     public String getGreeting() {
+        log.info("Fetching the greeting message from the catalog");
         return bookshopProperties.getGreeting();
     }
 }
