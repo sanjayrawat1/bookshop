d485c66696f6a4dfdb953351897b58c9fd447b10
Sanjay Singh Rawat <s.rawat1339@gmail.com>
2023-03-30 13:35:33 +0530

    edge-service: configure log pattern and add logs

--- edge-service/src/main/java/com/github/sanjayrawat1/bookshop/edgeservice/user/UserController.java
+++ edge-service/src/main/java/com/github/sanjayrawat1/bookshop/edgeservice/user/UserController.java
@@ -1,5 +1,6 @@
 package com.github.sanjayrawat1.bookshop.edgeservice.user;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,11 +10,13 @@ import reactor.core.publisher.Mono;
 /**
  * @author Sanjay Singh Rawat
  */
+@Slf4j
 @RestController
 public class UserController {
 
     @GetMapping("/user")
     public Mono<User> getUser(@AuthenticationPrincipal OidcUser oidcUser) {
+        log.info("Fetching information about the currently authenticated user");
         var user = new User(oidcUser.getPreferredUsername(), oidcUser.getGivenName(), oidcUser.getFamilyName(), oidcUser.getClaimAsStringList("roles"));
         return Mono.just(user);
     }
--- edge-service/src/main/java/com/github/sanjayrawat1/bookshop/edgeservice/web/rest/HomeController.java
+++ edge-service/src/main/java/com/github/sanjayrawat1/bookshop/edgeservice/web/rest/HomeController.java
@@ -2,12 +2,16 @@ package com.github.sanjayrawat1.bookshop.edgeservice.web.rest;
 
 import com.github.sanjayrawat1.bookshop.edgeservice.config.BookshopProperties;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
+ * Home Controller.
+ *
  * @author Sanjay Singh Rawat
  */
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class HomeController {
@@ -16,6 +20,7 @@ public class HomeController {
 
     @GetMapping("/home")
     public String getGreeting() {
+        log.info("Fetching the greeting message from the edge service");
         return bookshopProperties.getGreeting();
     }
 }
