1ade995dbde54ab79610e1dc45ac117b6d82d00f
Sanjay Singh Rawat <s.rawat1339@gmail.com>
2023-03-31 18:09:54 +0530

    config-service: configure metrics with spring boot actuator and prometheus

--- config-service/build.gradle.kts
+++ config-service/build.gradle.kts
@@ -18,6 +18,7 @@ extra["springCloudVersion"] = "2022.0.1"
 dependencies {
     implementation("org.springframework.cloud:spring-cloud-config-server")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
+    runtimeOnly("io.micrometer:micrometer-registry-prometheus")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
