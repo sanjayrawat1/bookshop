8849a5d2c5ce6a33fa6485c10a820d035c527c10
Sanjay Singh Rawat <s.rawat1339@gmail.com>
2023-04-08 15:05:09 +0530

    catalog-service: upgrade to Spring Boot 3.0.5

--- catalog-service/build.gradle.kts
+++ catalog-service/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     java
-    id("org.springframework.boot") version "3.0.4"
+    id("org.springframework.boot") version "3.0.5"
     id("io.spring.dependency-management") version "1.1.0"
     id("com.diffplug.spotless") version "6.16.0"
 }
@@ -8,6 +8,8 @@ plugins {
 group = "com.github.sanjayrawat1.bookshop"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_19
+
+description = "Provides functionality for managing the books in the catalog."
 
 configurations {
     compileOnly {
@@ -19,7 +21,7 @@ repositories {
     mavenCentral()
 }
 
-extra["springCloudVersion"] = "2022.0.1"
+extra["springCloudVersion"] = "2022.0.2"
 extra["testcontainersVersion"] = "1.17.6"
 extra["testKeycloakVersion"] = "2.5.0"
 extra["otelVersion"] = "1.24.0"
@@ -34,12 +36,16 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
     implementation("org.flywaydb:flyway-core")
+
     runtimeOnly("org.postgresql:postgresql")
     runtimeOnly("io.micrometer:micrometer-registry-prometheus")
     runtimeOnly("io.opentelemetry.javaagent:opentelemetry-javaagent:${property("otelVersion")}")
+
     compileOnly("org.projectlombok:lombok")
+
     annotationProcessor("org.projectlombok:lombok")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
     testImplementation("org.testcontainers:junit-jupiter")
